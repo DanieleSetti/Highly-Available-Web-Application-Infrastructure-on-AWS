2b56fe6c036eb32748827624e5dbe60fff1b620d
DanieleKostinSetti <danielekostin.setti@studenti.unimi.it>
2026-04-07 16:06:50 +0200

    Backend & RDS connection: Updated

--- app/backend.js
+++ app/backend.js
@@ -1,20 +1,26 @@
+
 const express = require('express');
 const { Client } = require('pg');
 
 const app = express();
 
 const client = new Client({
-    host: 'RDSHOST',
+    host: process.env.RDSHOST,
     user: 'postgres',
-    password: 'DB_PASSWORD',
+    password: process.env.DB_PASSWORD,
     port: 5432,
+    ssl: {
+      rejectUnauthorized: false
+    }
 });
 
 client.connect();
 
 app.get('/visit', async (req, res) => {
-    const result = await client.query('UPDATE counter SET value = value + 1 RETURN value');
+    const result = await client.query('UPDATE counter SET value = value + 1 RETURNING value');
     res.json({ visit: result.rows[0].value });
 })
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
